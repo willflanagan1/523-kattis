44f6ff70c81e2a0c32c8cd9a0558b3dca24c0864
Casey Joseph Stamper <cstamper@ada.cs.unc.edu>
2021-03-16 20:10:09 -0400

    more 'problem-package' problems

--- mypoll/src/COMP523/dockerClient.py
+++ mypoll/src/COMP523/dockerClient.py
@@ -1,10 +1,13 @@
 import docker
 import os
+import time
 
 client = docker.from_env()
 
 volume = {os.getcwd() + '/kattis': {'bind': '/kattis', 'mode': 'rw'}}
 
-container = client.containers.run('problemtools/icpc', 'ls /kattis/problems', volumes=volume, detach=True)
+container = client.containers.run('problemtools/icpc', 'verifyproblem /kattis/problems/guess', volumes=volume, detach=True)
 
-print(container.logs())
+while True:
+    time.sleep(25)
+    print(container.logs())
