d100930d75d95000bc775b97c88bc008f8bd20c8
Brendan Carr <brendo@ada.cs.unc.edu>
2021-03-22 15:19:23 -0400

    added brendo as port 8093

--- mypoll/src/appbase.py
+++ mypoll/src/appbase.py
@@ -236,6 +236,8 @@ def serve(test=True):
        port = '8091'
     elif getpass.getuser() == 'cstamper':
         port = "8092"
+    elif getpass.getuser() == 'brendo':
+        port = '8093'
     else:
        port = '8087'
     server.serve(port=port, host="0.0.0.0", restart_delay=2)
