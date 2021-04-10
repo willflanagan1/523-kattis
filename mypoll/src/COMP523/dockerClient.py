4834c0fcabb0322a48c8dbb5d3d5043d7d5154af
Casey Joseph Stamper <cstamper@ada.cs.unc.edu>
2021-04-10 15:39:09 -0400

    new homepage, f strings

--- mypoll/src/COMP523/dockerClient.py
+++ mypoll/src/COMP523/dockerClient.py
@@ -7,13 +7,9 @@ image = 'problemtools/icpc'
 volume = {os.getcwd() + '/kattis': {'bind': '/kattis', 'mode': 'rw'}}
 
 def verify_problem(name):
-    command = 'verifyproblem /kattis/problems/' + name
+    command = f'verifyproblem /kattis/problems/{name}'
     container = client.containers.run(image, command, volumes=volume, detach=True)
     time.sleep(15)
     return container.logs()
-
-def problem_2_html(name):
-    command = 'problem2html /kattis/problems/' + name
-    container = client.containers.run(image, command, volumes=volume, detach=True)
 
 print(verify_problem("hello"))
